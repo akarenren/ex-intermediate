c5664c054409e853828e62f18abc8fbc6531fe8e
akaseren <ren.akase@rakus.co.jp>
2020-06-23 11:28:51 +0900

    hotelコントローラ修正

--- src/main/java/com/example/controller/HotelController.java
+++ src/main/java/com/example/controller/HotelController.java
@@ -1,5 +1,50 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Hotel;
+import com.example.service.HotelService;
+
+@Controller
+@RequestMapping("/hotel")
 public class HotelController {
 
+	@Autowired
+	private HotelService hotelService;
+	
+	/**
+	 * ホテル検索入力画面表示.
+	 * 
+	 * @return ホテル検索入力画面
+	 */
+	@RequestMapping("")
+	public String index() {
+		return "hotel-index";
+	}
+	
+	/**
+	 * ホテル検索結果画面表示.
+	 * 
+	 * @param price:価格
+	 * @param model:モデル
+	 * @return　検索結果画面
+	 */
+	@RequestMapping("/search")
+	public String search(Integer price, Model model) {
+		List<Hotel> hotelList;
+		if(price == null) {
+			hotelList = hotelService.showNarrowList(price);
+		} else {
+			hotelList = hotelService.showList();
+		}
+		
+		model.addAttribute("hotelList", hotelList);
+		
+		return "hotel-index";
+	}
 }
--- src/main/java/com/example/service/HotelService.java
+++ src/main/java/com/example/service/HotelService.java
@@ -1,5 +1,43 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.Hotel;
+import com.example.repository.HotelRepository;
+
+/**
+ * Hotelを操作するサービス
+ * 
+ * @author ren.akase
+ *
+ */
+@Service
 public class HotelService {
 
+	@Autowired
+	private HotelRepository hotelRepository;
+	
+	/**
+	 * hotel一覧を取得.
+	 * 
+	 * @return hotel一覧
+	 */
+	public List<Hotel> showList() {
+		List<Hotel> hotelList = hotelRepository.findAll();
+		return hotelList;
+	}
+	
+	/**
+	 * 入力された価格以下のホテルを取得.
+	 * 
+	 * @param inputPrice 入力された価格
+	 * @return　条件に一致するホテル
+	 */
+	public List<Hotel> showNarrowList(Integer inputPrice) {
+		List<Hotel> narrowHotelList = hotelRepository.findByPrice(inputPrice);
+		return narrowHotelList;
+	}
 }
